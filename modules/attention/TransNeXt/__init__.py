61836b2ed95a7896187d7463a5c0b608987f0df0
志辰 杨 <oringec@163.com>
2024-08-01 20:21:54 +0800

    1. 添加 TransNeXt 中的 Aggregated Attention 模块

---
+++ modules/attention/TransNeXt/__init__.py
@@ -0,0 +1,8 @@
+
+"""
+TransNeXt: Robust Foveal Visual Perception for Vision Transformers
+
+论文链接 2023：https://arxiv.org/abs/2311.17132
+参考文章：https://blog.csdn.net/athrunsunny/article/details/135233826
+"""
+
